84583de8222696ace5df287e6075fdd4f1cbbb9b
anrumo <anrumo232@gmail.com>
2024-05-18 11:22:08 -0500

    Pagina de inicio sin header

---
+++ src/componentes/PaginaInicio.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../estilos/InicioStyle.css"
+
+function PaginaInicio(){
+
+    return(
+        <div className="Contenido">
+            <div>
+                <img src="https://cdn-icons-png.flaticon.com/512/3226/3226228.png" className="Bola"/>
+            </div>
+            <div className="Text-Inicio">
+                Nadie vuelve a ser el mismo después de bailar una noche en Moralma Dixco
+            </div>
+
+        </div>
+    );
+}
+
+export default PaginaInicio;
